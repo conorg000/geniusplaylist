acf29e9a4f2c6ee8edd8b6174cedd087a44fc152
Conor Gould <conor.gould@griffithuni.edu.au>
2019-06-15 18:43:34 +0800

    lib_data func now retrieves 1000 songs

--- lib_data.py
+++ lib_data.py
@@ -4,13 +4,15 @@ def lib_data(tkn):
   # Create a DF to put data in
   df = pd.DataFrame(columns=['track', 'artist', 'popularity', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness',
                  'liveness', 'valence', 'tempo'])
-  results = sp.current_user_saved_tracks(limit=50)
-  for i, obj in enumerate(results['items']):
-    item = obj['track']
-    track = item['name']
-    artist = item['artists'][0]['name']
-    pop = item['popularity']
-    analysis = sp.audio_features(item['id'])
-    data = list(analysis[0].values())[:11]
-    df.loc[i] = [track] + [artist] + [pop] + data
+  for n in range(0, 20):
+    results = sp.current_user_saved_tracks(offset=n*50, limit=50)
+    #print(len(results))
+    for i, obj in enumerate(results['items']):
+      item = obj['track']
+      track = item['name']
+      artist = item['artists'][0]['name']
+      pop = item['popularity']
+      analysis = sp.audio_features(item['id'])
+      data = list(analysis[0].values())[:11]
+      df.loc[i+(n*50)] = [track] + [artist] + [pop] + data
   return(df)
