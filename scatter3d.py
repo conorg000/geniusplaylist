b1956ebd037f80c96ccecf473bf65ee6f21eccbe
Conor Gould <conor.gould@griffithuni.edu.au>
2019-06-17 13:54:11 +0800

    added labels to data points, changed axes titles

--- scatter3d.py
+++ scatter3d.py
@@ -11,13 +11,14 @@ py.tools.set_credentials_file(username='cgonthegc', api_key='*******')
 
 # With help from plotly
 scatter = dict(mode = 'markers', name = 'y', type = 'scatter3d',
-              x=x, y=y, z=z, marker=dict(size=2, color='rgb(23,190,207)'))
-clusters = dict(alphahull=7, name='y', opacity=0.1, type='mesh3d',
-               x=x, y=y, z=z)
-layout = dict(title='3D Clustering of My Songs', scene=dict(xaxis=dict(zeroline=False),
-                                                           yaxis=dict(zeroline=False),
-                                                           zaxis=dict(zeroline=False),
+              x=x, y=y, z=z, text=np.array(music['track']), marker=dict(size=2, color='rgb(0,200,0)'))
+#clusters = dict(alphahull=7, name='y', opacity=0.1, type='mesh3d',
+#               x=x, y=y, z=z)
+layout = dict(title='3D Scatter of My Songs', scene=dict(xaxis=dict(title='energy',zeroline=False),
+                                                           yaxis=dict(title='valence',zeroline=False),
+                                                           zaxis=dict(title='tempo',zeroline=False),
                                                            ))
-fig = dict(data=[scatter, clusters], layout=layout)
-py.iplot(fig, filename='3D Clustering of My Songs')
+#fig = dict(data=[scatter, clusters], layout=layout)
+fig = dict(data=[scatter], layout=layout)
+py.iplot(fig, filename='3D Scatter of My Songs')
 
